5202b28e61bf82568850711ba38e7ca2e9c400b9
solodezaldivar <ramon.solodezaldivar@uzh.ch>
2020-04-17 18:56:47 +0200

    login fix final

--- src/main/java/ch/uzh/ifi/seal/soprafs20/rest/dto/LoginPutDTO.java
+++ src/main/java/ch/uzh/ifi/seal/soprafs20/rest/dto/LoginPutDTO.java
@@ -6,6 +6,10 @@ public class LoginPutDTO {
 
     private String password;
 
+    private Long id;
+
+    private String token;
+
     public String getUsername() {
         return username;
     }
@@ -17,4 +21,17 @@ public class LoginPutDTO {
     public void setPassword(String password){this.password = password;}
 
     public String getPassword(){return password;}
+
+    public String getToken() {
+        return token;
+    }
+    public void setToken(String token) {
+        this.token = token;
+    }
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
