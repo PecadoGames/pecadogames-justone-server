092b1608bd0299cc80039d61657f9f47087f4828
Marion Dübendorfer <marion@duebendorfer.ch>
2020-04-25 14:09:34 +0200

    :construction: S10/B/T3 add player entity

---
+++ src/main/java/ch/uzh/ifi/seal/soprafs20/entity/Player.java
@@ -0,0 +1,90 @@
+package ch.uzh.ifi.seal.soprafs20.entity;
+
+import ch.uzh.ifi.seal.soprafs20.constant.AvatarColor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+
+@Entity
+@Table(name = "PLAYER")
+public class Player {
+
+    @Id
+    private Long id;
+
+    @NotBlank
+    @NotEmpty
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = true, unique = true)
+    private String token;
+
+    @Column
+    private AvatarColor avatarColor;
+
+    @Column
+    private boolean guesser;
+
+    @Column
+    private boolean clueIsSent;
+
+    @Column
+    private int score;
+
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public AvatarColor getAvatarColor() {
+        return avatarColor;
+    }
+
+    public void setAvatarColor(AvatarColor avatarColor) {
+        this.avatarColor = avatarColor;
+    }
+
+    public boolean isClueIsSent() {
+        return clueIsSent;
+    }
+
+    public void setClueIsSent(boolean clueIsSent) {
+        this.clueIsSent = clueIsSent;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+
+}
