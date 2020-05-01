efb3f4af4a3b154948f4565fcc35d0ff20a53111
Marion Dübendorfer <marion@duebendorfer.ch>
2020-05-01 15:34:41 +0200

    :white_check_mark: add ChatRepositoryIntegrationTest

--- src/main/java/ch/uzh/ifi/seal/soprafs20/repository/ChatRepository.java
+++ src/main/java/ch/uzh/ifi/seal/soprafs20/repository/ChatRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface ChatRepository extends JpaRepository<Chat, Long> {
-    Optional<Chat> findById(long lobbyId);
+    Optional<Chat> findByLobbyId(long lobbyId);
 }
